c3baecb503bd05a82c61a87cf98c93cf0131f5d3
Sergey Tereshchenko <serghh1988@gmail.com>
2021-05-16 22:41:53 +0300

    Установил все базовые зависимости. Установил пакет axios. Собрал шаблон карточки страны с помощью шаблонизатора handlebars. Написал базовую верстку html.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './styles.css';
+const axios = require('axios');
+
+const refs = {
+  searchInput: document.querySelector('.search-input'),
+};
+
+// fetch('https://restcountries.eu/rest/v2/name/russia')
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(country => {
+//     console.log(country);
+//   });
+
+axios.defaults.baseURL = 'https://restcountries.eu/rest/v2';
+
+axios
+  .get('/name/switzerland')
+  .then(response => console.group(response))
+  .catch(error => console.log(error));
